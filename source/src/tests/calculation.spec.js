cddc175036bbdf910b0f0c2af9922a526da66155
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2023-12-25 13:02:44 +0100

    chore: Add averages test

--- source/src/tests/calculation.spec.js
+++ source/src/tests/calculation.spec.js
@@ -7,27 +7,27 @@ import path from "path";
 describe("statistics", () => {
   test("should look like expected", async () => {
     // Given
-    const dataDir = process.env["DATA_DIR"]
-    const categoryMapping = JSON.parse(fs.readFileSync(path.join(dataDir, "categories.json"), "utf8"));
+    const categoryMapping = JSON.parse(fs.readFileSync(path.join(__dirname, "../routes/categories/categories.json"), "utf8"));
+
     const data = fs.readFileSync(path.join(__dirname, "test.csv"), "utf8");
     const csv = d3.csvParse(data);
 
     // When
     const result = await statistics.calculate(d3, categoryMapping, csv);
-
+''
     // Then
     // // Note two gotchas
     // - the non-breaking space " ". This is intented behavior because implementation uses Intl.NumberFormat.
     // - the minus sign is "−" not "-". This is intented behavior because implementation uses Intl.NumberFormat.
-    const expectedHeader = ["Category", "2022-01", "2022-02", "2022-03", "Sum"];
+    const expectedHeader = ["Category", "2022-01", "2022-02", "2022-03", "Sum", "Average"];
 
     const expectedTableData = [
-      ["Personlig forbruk ➡ Sport og fritid", "−10 150", "0", "0", "−10 150"],
-      ["Reise ➡ Reise og transport", "0", "−60", "−39", "−99"],
-      ["Ukjent kategori", "0", "0", "0", "0"]
+      ["Personlig forbruk ➡ Sport og fritid", "−10 150", "0", "0", "−10 150", "−3 383"],
+      ["Reise ➡ Reise og transport", "0", "−60", "−39", "−99", "−33"],
+      ["Ukjent kategori", "0", "0", "0", "0", "0"]
     ];
 
-    const expectedFooter = ["Sum", "−10 150", "−60", "−39", "−10 249"];
+    const expectedFooter = ["Sum", "−10 150", "−60", "−39", "−10 249", "−3 416"];
 
     console.log(result.tableData);
 
