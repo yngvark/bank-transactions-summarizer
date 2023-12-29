3c2b719a26dca89a4daf2dd3b69b313c3307b542
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2023-12-29 21:19:43 +0100

    chore: Remove unused stuff

--- source/src/app.js
+++ source/src/app.js
@@ -1,5 +1,4 @@
 import express from "express";
-import transactionsHandler from "./routes/transactions/handler";
 import categoriesHandler from "./routes/categories/handler";
 import aiHandler from "./routes/ai/handler";
 import path from "path";
@@ -9,30 +8,7 @@ const app = express()
 let publicDir = path.join(__dirname, 'public')
 app.use(express.static(publicDir));
 
-app.use('/transactions', transactionsHandler)
 app.use('/categories', categoriesHandler)
 app.use('/ai', aiHandler)
 
 export default app
-
-// ---------------------------------
-// Commented stuff
-// ---------------------------------
-
-// Apply most middleware first
-/*
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cors({
-    origin: config.clientOrigins[config.nodeEnv]
-}))
-app.use(helmet())
-app.use(morgan('tiny'))
-
-// Apply routes before error handling
-app.use('/', root)
-
-// Apply error handling last
-app.use(fourOhFour)
-app.use(errorHandler)
-*/
