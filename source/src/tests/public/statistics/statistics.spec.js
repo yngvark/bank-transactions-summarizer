a8d3a9c0b3bae0f879a651a55d3022124398b916
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2024-01-21 10:35:50 +0100

    fix: Fix statistics.spec.js

--- source/src/tests/public/statistics/statistics.spec.js
+++ source/src/tests/public/statistics/statistics.spec.js
@@ -1,38 +1,27 @@
 import statistics from "../../../public/statistics/statistics.js";
-import parser from "../../../public/statistics/parser.js";
 
 import * as d3 from "../../d3-7.8.4.js";
 
-const fs = require("fs");
-import path from "path";
-
 describe("statistics", () => {
   test("should look like expected", async () => {
-    // Given
-    const categoriesFile = "../../../routes/categories/categories.json"
-    const categoryMapping = JSON.parse(fs.readFileSync(path.join(__dirname, categoriesFile), "utf8"));
-
-    const csvFile = "test.csv";
-    const data = fs.readFileSync(path.join(__dirname, csvFile), "utf8");
-    const transactions = d3.csvParse(data);
-    const transactionsWithCategory = await parser.parse(categoryMapping, transactions);
-
     // When
-    const result = await statistics.calculateTable(d3, transactionsWithCategory);
+    const result = await statistics.calculateTable(d3, testTransactionsWithCategory);
 
     // Then
     // // Note two gotchas
     // - the non-breaking space " ". This is intented behavior because implementation uses Intl.NumberFormat.
     // - the minus sign is "−" not "-". This is intented behavior because implementation uses Intl.NumberFormat.
-    const expectedHeader = ["Category", "2022-01", "2022-02", "2022-03", "Sum", "Average"];
+    const expectedHeader =
+        ["Category",                              "2022-01",    "2022-05",       "Sum",           "Average"];
 
     const expectedTableData = [
-      ["Personlig forbruk ➡ Sport og fritid", "−10 150", "0", "0", "−10 150", "−3 383"],
-      ["Reise ➡ Reise og transport", "0", "−60", "−39", "−99", "−33"],
-      ["Ukjent kategori", "0", "0", "0", "0", "0"]
+      ["Personlig forbruk ➡ Sport og fritid",     "−10 050", "−300",          "−10 350",    "−5 175"],
+
+      ["Reise ➡ Reise og transport",              "−24",        "0",             "−24",           "−12"],
     ];
 
-    const expectedFooter = ["Sum", "−10 150", "−60", "−39", "−10 249", "−3 416"];
+    const expectedFooter =
+        ["Sum",                                   "−10 074", "−300",          "−10 374", "−5 187"];
 
     console.log(result.tableData);
 
@@ -40,4 +29,63 @@ describe("statistics", () => {
     expect(result.tableData).toEqual(expectedTableData);
     expect(result.footer).toEqual(expectedFooter);
   });
+
+  const testTransactionsWithCategory = [
+    {
+      "TransactionDate": new Date("2022-01-01T00:00:00.000Z"),
+      "Text": "HELLO GOLFKLUBB",
+      "Type": "Kjøp",
+      "Currency Amount": -10000,
+      "Currency Rate": 1,
+      "Currency": "NOK",
+      "Amount": -10000,
+      "Merchant Area": "OSLO         ",
+      "Merchant Category": "Membership Clubs (Sports, Recreation",
+      "BookDate": new Date("2022-01-01T00:00:00.000Z"),
+      "ValueDate": new Date("2022-01-01T00:00:00.000Z"),
+      "Category": "Personlig forbruk ➡ Sport og fritid"
+    },
+    {
+      "TransactionDate": new Date("2022-01-01T00:00:00.000Z"),
+      "Text": "OTHER GOLFKLUBB",
+      "Type": "Kjøp",
+      "Currency Amount": -50,
+      "Currency Rate": 1,
+      "Currency": "NOK",
+      "Amount": -50,
+      "Merchant Area": "OSLO         ",
+      "Merchant Category": "Membership Clubs (Sports, Recreation",
+      "BookDate": new Date("2022-01-01T00:00:00.000Z"),
+      "ValueDate": new Date("2022-01-01T00:00:00.000Z"),
+      "Category": "Personlig forbruk ➡ Sport og fritid"
+    },
+    {
+      "TransactionDate": new Date("2022-01-01T00:00:00.000Z"),
+      "Text": "VOISCOOTERS",
+      "Type": "Kjøp",
+      "Currency Amount": -23.78,
+      "Currency Rate": 1,
+      "Currency": "NOK",
+      "Amount": -23.78,
+      "Merchant Area": "OSLO         ",
+      "Merchant Category": "Transportation Services, Not elsewhe",
+      "BookDate": new Date("2022-01-01T00:00:00.000Z"),
+      "ValueDate": new Date("2022-01-01T00:00:00.000Z"),
+      "Category": "Reise ➡ Reise og transport"
+    },
+    {
+      "TransactionDate": new Date("2022-05-01T23:00:00.000Z"),
+      "Text": "HELLO GOLFKLUBB",
+      "Type": "Kjøp",
+      "Currency Amount": -300,
+      "Currency Rate": 1,
+      "Currency": "NOK",
+      "Amount": -300,
+      "Merchant Area": "OSLO         ",
+      "Merchant Category": "Membership Clubs (Sports, Recreation",
+      "BookDate": new Date("2022-05-01T23:00:00.000Z"),
+      "ValueDate": new Date("2022-05-01T23:00:00.000Z"),
+      "Category": "Personlig forbruk ➡ Sport og fritid"
+    }
+  ]
 });
